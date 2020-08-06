01b83fd0f19ebc2c4e19fdc73ac06d07dc96d495
aayush-sinha <aayush_sinha@icloud.com>
2020-08-07 00:18:28 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useInput } from "./input-hook";
+import Words from "./Words";
 
 function App() {
+  const { value, bind, reset } = useInput("");
+  const [isVisible, setVisible] = useState(false);
+  const [wordlist, setWordList] = useState([]);
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("num", value);
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: "follow",
+    };
+
+    var result = await fetch("http://localhost:5000/num", requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        return result;
+      })
+      .catch((error) => console.log("error", error));
+    setWordList(result);
+    setVisible(true);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Enter Number
         </a>
+        <div className="container">
+          <form onSubmit={handleSubmit}>
+            <div className="row justify-content-center">
+              <div className="form-group col-6">
+                <input
+                  type="number"
+                  className="form-control num_input"
+                  placeholder="Enter number"
+                  {...bind}
+                />
+              </div>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
       </header>
+      {isVisible ? <Words wordlist={wordlist} /> : <p></p>}
     </div>
   );
 }
